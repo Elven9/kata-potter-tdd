ef3586332ce6566b2a2dd0c5fdfada038ef5fd93
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-05-14 15:07:51 +0800

    feat: implement basic price calculation to meet the requirement

--- potter.go
+++ potter.go
@@ -7,5 +7,18 @@ func BuyHarryPotter(
 	s4 int,
 	s5 int,
 ) float32 {
-	return 0
+
+	totalBooks := s1 + s2 + s3 + s4 + s5
+	fullPrice := float32(totalBooks) * 8
+	if totalBooks >= 5 {
+		return fullPrice * 0.75
+	} else if totalBooks >= 4 {
+		return fullPrice * 0.80
+	} else if totalBooks >= 3 {
+		return fullPrice * 0.90
+	} else if totalBooks >= 2 {
+		return fullPrice * 0.95
+	} else {
+		return fullPrice
+	}
 }
