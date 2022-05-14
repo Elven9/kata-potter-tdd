e7a2e2fe60245dfb55b6d92e1154363220d153e1
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-05-14 17:17:20 +0800

    refactor: use array as new data structure for calculation

--- potter.go
+++ potter.go
@@ -9,47 +9,33 @@ func BuyHarryPotter(
 ) float32 {
 
 	var result float32 = 0
+	// Initialize Data Structure
+	requestBooks := []int{s1, s2, s3, s4, s5}
 
 	// Scan all five books count
 	for {
 		var tmp float32
 		totalBooks := 0
-		// Count Books
-		if s1 > 0 {
-			totalBooks += 1
-			s1 -= 1
+		for i := 0; i < len(requestBooks); i++ {
+			if requestBooks[i] > 0 {
+				totalBooks += 1
+				requestBooks[i] -= 1
+			}
 		}
 
-		if s2 > 0 {
-			totalBooks += 1
-			s2 -= 1
-		}
-
-		if s3 > 0 {
-			totalBooks += 1
-			s3 -= 1
-		}
-
-		if s4 > 0 {
-			totalBooks += 1
-			s4 -= 1
-		}
-
-		if s5 > 0 {
-			totalBooks += 1
-			s5 -= 1
-		}
-
-		if totalBooks >= 5 {
+		switch totalBooks {
+		case 5:
 			tmp = 40 * 0.75
-		} else if totalBooks >= 4 {
+		case 4:
 			tmp = 32 * 0.80
-		} else if totalBooks >= 3 {
+		case 3:
 			tmp = 24 * 0.90
-		} else if totalBooks >= 2 {
+		case 2:
 			tmp = 16 * 0.95
-		} else {
-			tmp = float32(totalBooks) * 8
+		case 1:
+			tmp = 8
+		default:
+			tmp = 0
 		}
 		if tmp == 0 {
 			break
@@ -57,6 +43,5 @@ func BuyHarryPotter(
 			result += tmp
 		}
 	}
-
 	return result
 }
